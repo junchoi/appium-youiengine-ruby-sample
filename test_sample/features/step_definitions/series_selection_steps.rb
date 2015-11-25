09e4efe3812913bf73de739dd64594ac420ab4ee
Dave Rooney <dave.rooney@youi.tv>
2015-11-25 10:59:22 -0500

    Initial scenario and skeleton steps

---
+++ test_sample/features/step_definitions/series_selection_steps.rb
@@ -0,0 +1,19 @@
+Given(/^that onboarding has already been completed$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I am in the environment$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I tap on Bubble Guppies$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I see a Loading screen$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I see the player with Bubble Guppies$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
